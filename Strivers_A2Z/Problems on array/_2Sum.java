147a0153e2adad0fc7ec050fff72e6082dd09a7a
Sambhav <stanay929@gmail.com>
2025-09-17 19:26:18 +0530

    Sum of two nuymber

---
+++ Strivers_A2Z/Problems on array/_2Sum.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class _2Sum {
+    private static boolean check(int[] array, int s, int tar) {
+        for (int i = 0; i < s - 1; i++) {
+            for (int j = 0; j < s - 1; j++) {
+                int var = array[i] + array[j];
+                if (var == tar) {
+                    return true;
+
+                }
+            }
+        }
+        return false;
+    }
+
+    private static int[] Sum(int[] array, int s, int tar) {
+        _2Sum obj = new _2Sum();
+        if (obj.check(array, s, tar) == false) {
+            return new int[] { -1, -1 };
+        } else {
+            for (int i = 0; i < s - 1; i++) {
+                for (int j = 0; j < s - 1; j++) {
+                    int var = array[i] + array[j];
+                    if (var == tar) {
+                        return new int[] { i, j };
+
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the size of array  : ");
+        int n = in.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the elements in the array : ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        System.out.println("Enter the Target Element  : ");
+        int t = in.nextInt();
+
+        int arr1[] = Sum(arr, n, t);
+        System.out.println(
+                "Array after Left Rotation is : " + Arrays.toString(arr1));
+    }
+}
---
+++ Strivers_A2Z/Problems on array/source.java
@@ -0,0 +1,66 @@
+    import java.util.*;
+    public class source
+    {
+
+        private static boolean check(int[] arr , int t)
+        {
+            //Checking if the sum of two numbers = target ?
+
+            for (int i = 0; i < arr.length; i++)
+            {
+            for (int j = 0; j < arr.length; j++)
+            {
+                if((arr[i]+arr[j])==t)
+                return true;   
+            }   
+            }
+            return false ;
+        }
+
+        private static int[] comp(int arr[], int t)
+        {
+            int newarr[]=new int[2];
+            while(check(arr,t))
+            {
+                for (int i = 0; i < arr.length; i++)
+                {
+                    for (int j = 1; j < arr.length; j++) {
+                        if((arr[i]+arr[j])==t)
+                        {
+                            return new int[]{i,j};
+                        }
+                    }
+                }
+            }
+
+            int[] flagarr = {-1,-1};
+            return flagarr;
+        }
+        
+        
+        public static void main(String[] args)
+        {
+            Scanner in = new Scanner (System.in);
+            System.out.println("Enter the target Element : ");
+            int tar=in.nextInt();
+            System.out.println("Enter the size : ");
+            int n = in.nextInt();
+            int arr[]=new int[n];
+            System.out.println("Enter the elements : ");
+            for (int i = 0; i < n; i++)
+            {
+                arr[i]=in.nextInt();
+            }
+
+            boolean chk1 = check(arr,tar);
+            System.out.println(chk1);
+            if(chk1==false)
+            {
+            System.out.println("NO");
+            }
+            else
+            System.out.println("YES");
+            int a[]=comp(arr, tar);    
+            System.out.println(Arrays.toString(a));
+        }
+    }
